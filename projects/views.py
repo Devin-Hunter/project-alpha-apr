6f408c95508367a8d10206a3c8c8801b8655ad51
Devin-Matherne <devinmatherne@ymail.com>
2023-12-20 16:15:23 -0700

    Feature 13 Complete

--- projects/views.py
+++ projects/views.py
@@ -12,3 +12,10 @@ def list_projects(request):
         "list_of_projects": list_of_projects,
     }
     return render(request, "projects/list_projects.html", context)
+
+
+@login_required
+def show_project(request, id):
+    project_details = Project.objects.get(id=id)
+    context = {"project_details": project_details}
+    return render(request, "projects/project_details.html", context)
