15b88e0fd929147beaaf312b1de2af08d3670be2
Devin-Matherne <devinmatherne@ymail.com>
2023-12-20 16:23:13 -0700

    Feature 14 Complete

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
 from projects.models import Project
-
+from projects.forms import CreateProjectForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
@@ -19,3 +19,18 @@ def show_project(request, id):
     project_details = Project.objects.get(id=id)
     context = {"project_details": project_details}
     return render(request, "projects/project_details.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = CreateProjectForm(request.POST)
+        if form.is_valid():
+            form = form.save()
+            return redirect("list_projects")
+    else:
+        form = CreateProjectForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "projects/create_project.html", context)
